ed89bc5551f853e2c9152e64429e2c5bc17484b6
amir.karimi6610 <iamthetallboy>
2018-10-21 20:34:24 +0330

    added some tests for bookMeeting and notifyMeetings

--- Tests/MeetingManagerTest.java
+++ Tests/MeetingManagerTest.java
@@ -1,8 +1,12 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.IOException;
 import java.util.Date;
 import java.util.Vector;
+
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertTrue;
 
 public class MeetingManagerTest {
 
@@ -30,13 +34,41 @@ public class MeetingManagerTest {
         attendees1.add(p5);
         m1 = new Meeting(attendees1, new Date());
         m2 = new Meeting(attendees2, new Date());
-        db = new MockDB();
     }
 
     @Test(expected = Exception.class)
     public void bookMeeting_bookAMeetingWithBadDate_meetingNotSavedIntoDatabase() throws Exception {
+        db = new MockDB();
         MockMeetingManager meetingManager = new MockMeetingManager(db);
         meetingManager.setDateValidatorUrl("http://localhost:" + Utils.DateServerPort + "/false");
         meetingManager.bookMeeting(attendees1, 10);
+    }
+
+    @Test
+    public void bookMeeting_bookMeetingWithGoodDate_meetingSavedIntoDatabase() throws Exception{
+        db = new MockDB();
+        MockMeetingManager meetingManager = new MockMeetingManager(db);
+        meetingManager.setDateValidatorUrl("http://localhost:" + Utils.DateServerPort + "/true");
+        meetingManager.bookMeeting(attendees1, 10);
+        assertTrue(db.isCalled());
+    }
+
+    @Test
+    public void notifyMeetings_notifyDatabaseMeetings_notifyAUserInOneMeetingOnce() throws IOException{
+        db = new MockDB();
+        db.addMeeting(m1);
+        MockMeetingManager meetingManager = new MockMeetingManager(db);
+        meetingManager.notifyMeetings();
+        assertEquals(p1.getNotifiedCount(), 1);
+    }
+
+    @Test
+    public void notifyMeetings_notifyDatabaseMeetings_notifyAUserInTwoMeetingTwice() throws IOException{
+        db = new MockDB();
+        db.addMeeting(m1);
+        db.addMeeting(m2);
+        MockMeetingManager meetingManager = new MockMeetingManager(db);
+        meetingManager.notifyMeetings();
+        assertEquals(p3.getNotifiedCount(), 2);
     }
 }
--- Tests/MockDB.java
+++ Tests/MockDB.java
@@ -2,6 +2,11 @@ import java.util.Vector;
 
 public class MockDB {
     private Vector<Meeting> meetings = new Vector<>();
+    private boolean called = false;
+
+    public boolean isCalled() {
+        return called;
+    }
 
     public Vector<Meeting> getAllMeetings(){
         return this.meetings;
@@ -9,5 +14,6 @@ public class MockDB {
 
     public void addMeeting(Meeting meeting){
         meetings.add(meeting);
+        called = true;
     }
 }
